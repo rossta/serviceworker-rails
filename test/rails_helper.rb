4f4a01bf49e58c3413834b09552a91ce9988b002
Ross Kaffenberger <rosskaff@gmail.com>
2016-04-24 19:59:03 -0400

    Remove comments

--- test/rails_helper.rb
+++ test/rails_helper.rb
@@ -11,11 +11,3 @@ require "rails/test_help"
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-
-# # Load fixtures from the engine
-# if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-#   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-#   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-#   ActiveSupport::TestCase.fixtures :all
-# end
-
