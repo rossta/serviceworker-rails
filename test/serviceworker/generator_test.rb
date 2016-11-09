c199790e6507db881aab388c45b681880d699634
Ross Kaffenberger <rosskaff@gmail.com>
2016-11-09 17:43:14 -0500

    Suppress output of generator test commands

--- test/serviceworker/generator_test.rb
+++ test/serviceworker/generator_test.rb
@@ -5,20 +5,20 @@ module GeneratorTestHelpers
   def create_generator_sample_app
     FileUtils.mkdir_p(tmp_path)
     FileUtils.cd(tmp_path) do
-      system "rails new generator_sample --skip-active-record --skip-test-unit --skip-spring --skip-bundle 2>&1"
+      system "rails new generator_sample --skip-active-record --skip-test-unit --skip-spring --skip-bundle --quiet"
       File.open(File.join(sample_app_path, "Gemfile"), "a") do |f|
         f.puts "gem 'serviceworker-rails', path: '#{File.join(File.dirname(__FILE__), "..", "..")}'"
       end
     end
 
     FileUtils.cd(sample_app_path) do
-      system "bundle install 2>&1"
+      system "bundle install --quiet"
     end
   end
 
   def install_serviceworker_rails
     FileUtils.cd(sample_app_path) do
-      system "rails g serviceworker:install -f 2>&1"
+      system "rails g serviceworker:install --quiet -f 2>&1"
     end
   end
 
