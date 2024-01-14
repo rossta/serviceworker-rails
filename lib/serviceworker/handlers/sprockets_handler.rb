ae180e2e325552b214b9c8023fcb8e4866e143e1
Robert Keresnyei <rk@nemron.com>
2024-01-14 20:54:22 +0100

    Fix paths

--- lib/serviceworker/handlers/sprockets_handler.rb
+++ lib/serviceworker/handlers/sprockets_handler.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "rack/file"
+require "rack/files"
 
 module ServiceWorker
   module Handlers
--- lib/serviceworker/handlers/webpacker_handler.rb
+++ lib/serviceworker/handlers/webpacker_handler.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "rack/file"
+require "rack/files"
 require "webpacker"
 
 module ServiceWorker
