2e998c862b9d0d1c5ea5b65fa3845704f362fd1b
rutgerbogaerds <24680821+rutgerbogaerds@users.noreply.github.com>
2025-03-24 15:07:53 +0100

    on extra letter

--- lib/serviceworker/handlers/webpacker_handler.rb
+++ lib/serviceworker/handlers/webpacker_handler.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "rack/file"
+require "rack/files"
 require "webpacker"
 
 module ServiceWorker
@@ -22,7 +22,7 @@ module ServiceWorker
       private
 
       def file_server
-        @file_server ||= ::Rack::File.new(::Rails.public_path)
+        @file_server ||= ::Rack::Files.new(::Rails.public_path)
       end
     end
   end
